c84ea43967a918e64d5c579a6de69f97c044b894
chulkov <pilot114@bk.ru>
2020-08-21 20:45:09 +0700

    rm cache

--- tests/cache/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '1234510.231';
-$username = $_GET['username'] ?? 'default_username';
-$pass = $_GET['pass'] ?? 'default_pass';
-$auth = new Auth();
-$result = $auth->login($username, $pass);
-echo $result . "\n";
-$result = $auth->login('test', 'test');
-echo $result . "\n";
